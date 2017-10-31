e6b68696a8d778a340ad32c909a4759966f76a9f
Andre LaFleur <andre.lafleur@utah.edu>
2017-10-30 20:37:30 -0600

    Add data directory

--- src/main/java/main/cs5340/topaz_turtles/Main.java
+++ src/main/java/main/cs5340/topaz_turtles/Main.java
@@ -1,19 +1,25 @@
 package main.cs5340.topaz_turtles;
 
+import com.google.gson.reflect.TypeToken;
 import com.google.gson.Gson;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Type;
+import java.util.Scanner;
+import java.util.TreeMap;
 
 /**
  * The main class of the application.
  */
 public class Main {
+
+    public static final String LOCAL_DATA_FILEPATH = ".topaz_turtles_data/";
+    public static final String RELATED_WORDS_FILEPATH = LOCAL_DATA_FILEPATH + "related_words.json";
+
+    private static TreeMap<IncidentType, DataMuseWord[]> relatedWordsToEachIncident;
 
     public static void main(String[] args) {
         if (args.length == 0) {
@@ -21,8 +27,72 @@ public class Main {
             System.exit(0);
         }
 
-        DataMuseWord[] words = DataMuse.getWordsRelatedTo("arson");
-        for (DataMuseWord w : words)
-            System.out.println(w.word);
+        grabNecessaryData();
+    }
+
+    private static void grabNecessaryData() {
+        File path = new File(LOCAL_DATA_FILEPATH);
+
+        // Ensure that the local data directory exists
+        if (!path.exists()) {
+            System.out.println("Creating local data directory...");
+            boolean flag = false;
+            try {
+                flag = path.mkdir();
+            } catch (SecurityException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+
+            if (!flag) {
+                System.err.println("Could not create data directory!");
+                System.exit(0);
+            }
+        }
+
+        // Ensure that the related words file path exists
+        File relatedWordsFile = new File(RELATED_WORDS_FILEPATH);
+        if (!relatedWordsFile.exists()) {
+            relatedWordsToEachIncident = new TreeMap<IncidentType, DataMuseWord[]>();
+
+            System.out.println("Grabbing related words for each IncidentType...");
+            for (IncidentType t : IncidentType.values()) {
+                DataMuseWord[] words = DataMuse.getWordsRelatedTo(t.toString().toLowerCase());
+                relatedWordsToEachIncident.put(t, words);
+            }
+
+            Gson gson = new Gson();
+            String relatedWordsJson = gson.toJson(relatedWordsToEachIncident);
+
+            try {
+                System.out.println("Creating related words file...");
+                PrintWriter writer = new PrintWriter(RELATED_WORDS_FILEPATH, "UTF-8");
+                writer.print(relatedWordsJson);
+                writer.flush();
+                writer.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                System.exit(1);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        } else {
+            Gson gson = new Gson();
+
+            System.out.println("Grabbing related words...");
+            try {
+                StringBuilder builder = new StringBuilder();
+                Scanner s = new Scanner(relatedWordsFile);
+                while (s.hasNextLine())
+                    builder.append(s.nextLine());
+                s.close();
+
+                Type treeType = new TypeToken<TreeMap<IncidentType, DataMuseWord[]>>(){}.getType();
+                relatedWordsToEachIncident = gson.fromJson(builder.toString().trim(), treeType);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
